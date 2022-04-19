98bb24a9c8e19cf11919905689bcb8bd1d4b69a0
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2022-04-19 22:07:56 +0900

    크롤링 수정 1

---
+++ crawl/mouse_wheel.py
@@ -0,0 +1,26 @@
+from selenium.common.exceptions import WebDriverException
+
+def wheel_element(element, deltaY=120, offsetX=0, offsetY=0):
+    error = element._parent.execute_script("""
+        var element = arguments[0];
+        var deltaY = arguments[1];
+        var box = element.getBoundingClientRect();
+        var clientX = box.left + (arguments[2] || box.width / 2);
+        var clientY = box.top + (arguments[3] || box.height / 2);
+        var target = element.ownerDocument.elementFromPoint(clientX, clientY);
+
+        for (var e = target; e; e = e.parentElement) {
+        if (e === element) {
+            target.dispatchEvent(new MouseEvent('mouseover', {view: window, bubbles: true, cancelable: true, clientX: clientX, clientY: clientY}));
+            target.dispatchEvent(new MouseEvent('mousemove', {view: window, bubbles: true, cancelable: true, clientX: clientX, clientY: clientY}));
+            target.dispatchEvent(new WheelEvent('wheel',     {view: window, bubbles: true, cancelable: true, clientX: clientX, clientY: clientY, deltaY: deltaY}));
+            return;
+        }
+        }    
+        return "Element is not interactable";
+        """, element, deltaY, offsetX, offsetY)
+    if error:
+        raise WebDriverException(error)
+#        logger.debug("fail to scroll")
+
+    return
