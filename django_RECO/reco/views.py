295f2eb3c62b420d2aa9c2e8155e632697a8ff46
world970511 <57740138+world970511@users.noreply.github.com>
2022-05-23 20:48:28 +0900

    feat: reco api 작성(1)

--- django_RECO/reco/views.py
+++ django_RECO/reco/views.py
@@ -1,12 +1,14 @@
 from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import AllData
+import pandas as pd
+
 
 # Create your views here.
 def intro(request):
     return render(request, 'intro.html')
 
-#여기부터 작성할 것
+#데이터 선택
 def select(request):
     page = request.GET.get('page', '1')  # 페이지
     webtoons=AllData.objects.all()
@@ -17,12 +19,21 @@ def select(request):
         webtoon = paginator.page(1)
     except EmptyPage:
         webtoon = paginator.page(paginator.num_pages)
-    if request.POST: 
-        selected_webbtoon_id_list = list(map(int, request.POST.getlist('selected')))
-        print(selected_webbtoon_id_list)
+    if request.method == "POST":
+        selected_webbtoon_id_list = request.POST.getlist('selected') 
+        print(map(int,selected_webbtoon_id_list)) 
+        reco(selected_webbtoon_id_list,webtoons)
     return render(request, 'select.html',{'webtoon':webtoon})    
 
+#추천
+def reco(idList,all_data):
+    selected_data=AllData.objects.filter(id__in=idList)
+    all_df = pd.DataFrame(list(all_data))
+    selected_df=pd.DataFrame(list(AllData.objects.filter(id__in=idList)))
+    selected_data.to_csv('test.csv')
+    return redirect('reco:result')
 
-#추천 적용
+#추천 출력
 def result(request):
-    return render(request, 'result.html')
+    all_data=AllData.objects.all()
+    return render(request, 'result.html',{'all_data':all_data[:10]})
--- django_RECO/static/js/clickCards.js
+++
@@ -1,6 +0,0 @@
-$('form input[type="checkbox"]').click(function () {
-    var label = $('label[for="'+$(this).attr('id')+'"]');
-    $(label).animate({ 'margin': '0' }, 100);
-    $(label).animate({ 'margin': '5px' }, 100);
-  });
-
