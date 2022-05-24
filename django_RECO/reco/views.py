707cf6f28df63cd811dcb4812e197794fa49b958
world970511 <57740138+world970511@users.noreply.github.com>
2022-05-24 19:29:29 +0900

    feat: reco api 작성(2) - form 수정

--- django_RECO/reco/views.py
+++ django_RECO/reco/views.py
@@ -1,8 +1,7 @@
 from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from .models import AllData
 import pandas as pd
-
 
 # Create your views here.
 def intro(request):
@@ -19,21 +18,23 @@ def select(request):
         webtoon = paginator.page(1)
     except EmptyPage:
         webtoon = paginator.page(paginator.num_pages)
-    if request.method == "POST":
-        selected_webbtoon_id_list = request.POST.getlist('selected') 
-        print(map(int,selected_webbtoon_id_list)) 
-        reco(selected_webbtoon_id_list,webtoons)
+    if request.method == "POST":#체크된 데이터 가져오기
+        selected_webbtoon_id_list = list(map(int,request.POST.getlist('selected')))
+        reco(selected_webbtoon_id_list,webtoons)#추천
+        return redirect('reco:result')
     return render(request, 'select.html',{'webtoon':webtoon})    
 
 #추천
 def reco(idList,all_data):
+    recommendWebtoons=[]
     selected_data=AllData.objects.filter(id__in=idList)
-    all_df = pd.DataFrame(list(all_data))
-    selected_df=pd.DataFrame(list(AllData.objects.filter(id__in=idList)))
-    selected_data.to_csv('test.csv')
-    return redirect('reco:result')
+    all_df = pd.DataFrame(list(all_data.values()))
+    selected_df=pd.DataFrame(list(AllData.objects.filter(id__in=idList).values()))
+    print(selected_df)
+
 
 #추천 출력
 def result(request):
     all_data=AllData.objects.all()
+    print(all_data)
     return render(request, 'result.html',{'all_data':all_data[:10]})
