43517d1f3eb4b87d44c037f86e80ca3abab17619
world970511 <57740138+world970511@users.noreply.github.com>
2022-06-15 12:31:10 +0900

    heroku

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import decouple
 from django.core.exceptions import ImproperlyConfigured
 from decouple import config
 
