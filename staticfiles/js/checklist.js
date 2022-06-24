3393682d20098f68fbd7671c992b4750f611b9c9
world970511 <57740138+world970511@users.noreply.github.com>
2022-06-24 15:18:58 +0900

    error message

--- staticfiles/js/checklist.js
+++ staticfiles/js/checklist.js
@@ -1,14 +1,16 @@
 $("label img").on("click", function() {
-    $("#" + $(this).parents("label").attr("for")).click();
-  });
+  $("#" + $(this).parents("label").attr("for")).click();
+});
 
- $( 'input:checkbox[name="selected"]' ).on( "click", function() {
- if($( 'input:checkbox[name="selected"]:checked' ).length > 9)
-  {
-    $('#submitnBtn').removeAttr('disabled');
-  }
-  else
-  {
-    $('#submitnBtn').attr("disabled",true); 
-  }  
+$( 'input:checkbox[name="selected"]' ).on( "click", function() {
+if($( 'input:checkbox[name="selected"]:checked' ).length > 9)
+{
+  $('#submitnBtn').removeAttr('disabled');
+  $('#checkSelect').show();
+}
+else
+{
+  $('#submitnBtn').attr("disabled",true); 
+  $('#checkSelect').hide();
+}  
 });
